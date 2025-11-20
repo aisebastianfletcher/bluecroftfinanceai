162e372147e27561d9171987bbbbdfe27c57cfa1
aisebastianfletcher <aisebastianfletcher@gmail.com>
2025-11-20 15:38:01 -0100

    Add file upload handling functionality

--- app/upload_handler.py
+++ app/upload_handler.py
@@ -1,1 +1,8 @@
+import os
 
+def save_uploaded_file(uploaded_file, dest_dir="output/generated_pdfs"):
+    os.makedirs(dest_dir, exist_ok=True)
+    dest = os.path.join(dest_dir, uploaded_file.name)
+    with open(dest, "wb") as f:
+        f.write(uploaded_file.getbuffer())
+    return dest

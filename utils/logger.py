991e0285c46fa8429a93af2ea7af01b7ee25fd98
aisebastianfletcher <aisebastianfletcher@gmail.com>
2025-11-20 15:54:30 -0100

    Add logging setup to logger.py

--- utils/logger.py
+++ utils/logger.py
@@ -1,1 +1,10 @@
+import logging
 
+logger = logging.getLogger("ai_lending_assistant")
+logger.setLevel(logging.INFO)
+if not logger.handlers:
+    ch = logging.StreamHandler()
+    ch.setLevel(logging.INFO)
+    formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
+    ch.setFormatter(formatter)
+    logger.addHandler(ch)
